8df7c05c08c8ab121b81be01e97700da77bff258
James McDougall <jameswiliammcdougall@gmail.com>
2020-08-26 15:57:28 -0500

    Added onboarding view controllers

---
+++ AnywhereFitness/AnywhereFitness/Controllers/ViewControllers/Onboarding/PasswordViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PasswordViewController.swift
+//  AnywhereFitness
+//
+//  Created by Lambda_School_loaner_226 on 8/26/20.
+//  Copyright © 2020 LambdaSchool. All rights reserved.
+//
+
+import UIKit
+
+class PasswordViewController: UIViewController {
+    
+    @IBOutlet weak var createPassword: UITextField!
+    @IBOutlet weak var confirmPassword: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
