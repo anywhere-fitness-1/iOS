f9632ed8650367de808a288cf6049fb7504e42d1
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-08-25 21:55:34 -0700

    Networking - Incomplete

---
+++ AnywhereFitness/AnywhereFitness/Models/ClassListing.swift
@@ -0,0 +1,25 @@
+//
+//  ClassListing.swift
+//  AnywhereFitness
+//
+//  Created by Cora Jacobson on 8/25/20.
+//  Copyright © 2020 LambdaSchool. All rights reserved.
+//
+
+import Foundation
+
+struct ClassListing: Codable {
+    
+    var id: Int
+    var name: String
+    var type: String
+    var time: String
+    var date: String
+    var duration: String
+    var intensity: String
+    var location: String
+    var attendees: Int
+    var maxClassSize: Int
+    var instructorID: Int
+    
+}
